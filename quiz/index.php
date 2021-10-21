9341fd6a19f3413a4d643be28ea3b08ccaf62797
Zahin Zulkipli <zahin@Zahins-MacBook-Pro.local>
2021-10-21 14:02:48 +0800

    start quiz

---
+++ quiz/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Attendance</title>
+    <style>
+        html, body {
+            height: auto; 
+            width: 100%;
+            font-family: monospace;
+        }
+        img {
+            width: 100%;
+        }
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            overflow-wrap: anywhere;
+        }
+        hr {
+            margin-top: 30px;
+            border: none;
+            border-top: 1px solid gainsboro;
+        }
+        div {
+            width: 70%;
+            height: auto;
+            display: flex;
+            flex-direction: column;
+            justify-items: center;
+            align-items: flex-start;
+            border-bottom: 1px solid gainsboro;
+            padding-bottom: 50px;
+        }
+        .div-flex-center {
+            align-items: center;
+            text-align: center;
+        }
+        section {
+            margin-top: 10px;
+            background-color: #dcdcdc8a;
+            line-height: 1.5;
+        }
+    </style>
+</head>
+<body>
+    <div>
+        <p>Staff attendance system</p>
+        <section>
+            <?php
+
+            ?>
+        </section>
+    </div>
+</body>
+</html>
